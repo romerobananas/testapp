ba88a0012da20a80105f8ac0e0225e9555eff8d1
Feb <feb@MacBook-Pro-de-Feb.local>
2019-07-18 00:35:29 -0400

    Updates

---
+++ precache-manifest.1cb1d133532bd8eee307710ea3c59e07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b63cc02b2c3f6f388c9db9dad3fffa",
+    "url": "/Pondera/index.html"
+  },
+  {
+    "revision": "949637c983ce9cc4cda2",
+    "url": "/Pondera/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c1efb13d248482445e0d",
+    "url": "/Pondera/static/css/main.0ded2974.chunk.css"
+  },
+  {
+    "revision": "949637c983ce9cc4cda2",
+    "url": "/Pondera/static/js/2.3cb8812e.chunk.js"
+  },
+  {
+    "revision": "c1efb13d248482445e0d",
+    "url": "/Pondera/static/js/main.ff05a674.chunk.js"
+  },
+  {
+    "revision": "d7ec61fd910a004bc54d",
+    "url": "/Pondera/static/js/runtime~main.6cf35274.js"
+  },
+  {
+    "revision": "c317631a13b670a611995c67837fdeec",
+    "url": "/Pondera/static/media/button.c317631a.svg"
+  }
+]);
